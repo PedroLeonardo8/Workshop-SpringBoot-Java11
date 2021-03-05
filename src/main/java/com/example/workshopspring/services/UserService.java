5d98bfab336f5702a9b098afea3240f36f0340fc
PedroLeonardo8 <pedroleonardo841@gmail.com>
2021-03-05 14:43:57 -0300

    Exception Delete

--- src/main/java/com/example/workshopspring/services/UserService.java
+++ src/main/java/com/example/workshopspring/services/UserService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopspring.entities.User;
 import com.example.workshopspring.repositories.UserRepository;
+import com.example.workshopspring.services.exceptions.DatabaseException;
 import com.example.workshopspring.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -15,24 +19,30 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repositoty;
-	
-	public List <User> findAll(){
+
+	public List<User> findAll() {
 		return repositoty.findAll();
 	}
-	
+
 	public User findById(Long id) {
-	Optional<User> obj = repositoty.findById(id);
-	return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		Optional<User> obj = repositoty.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repositoty.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repositoty.deleteById(id);
+		try{
+			repositoty.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repositoty.getOne(id);
 		updateData(entity, obj);

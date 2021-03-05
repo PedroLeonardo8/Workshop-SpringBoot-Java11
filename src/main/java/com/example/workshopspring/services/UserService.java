c5d079b3b04dbac2d498ecfc4e67d8efb8be4ad1
PedroLeonardo8 <pedroleonardo841@gmail.com>
2021-03-05 14:53:37 -0300

    Exception Update

--- src/main/java/com/example/workshopspring/services/UserService.java
+++ src/main/java/com/example/workshopspring/services/UserService.java
@@ -3,8 +3,9 @@ package com.example.workshopspring.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
@@ -34,19 +35,23 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		try{
+		try {
 			repositoty.deleteById(id);
-		}catch(EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repositoty.getOne(id);
-		updateData(entity, obj);
-		return repositoty.save(entity);
+		try {
+			User entity = repositoty.getOne(id);
+			updateData(entity, obj);
+			return repositoty.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {

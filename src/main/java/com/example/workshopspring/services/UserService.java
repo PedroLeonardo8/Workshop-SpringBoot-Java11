41f68ce265be58c87e7c80342c4361299868ece2
PedroLeonardo8 <pedroleonardo841@gmail.com>
2021-03-09 15:37:33 -0300

    DataBase PostgreSQL

--- src/main/java/com/example/workshopspring/services/UserService.java
+++ src/main/java/com/example/workshopspring/services/UserService.java
@@ -50,7 +50,8 @@ public class UserService {
 			updateData(entity, obj);
 			return repositoty.save(entity);
 		} catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundException(id);
+			throw new 
+			ResourceNotFoundException(id);
 		}
 	}
 
